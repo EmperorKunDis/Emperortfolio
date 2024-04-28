734281a9751f39f4e56d0ca98be52f583b055504
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2024-04-28 21:32:48 +0000

    fdds

--- src/scripts/sectionFunction.js
+++ src/scripts/sectionFunction.js
@@ -1,10 +1,6 @@
 const aboutButton = document.getElementById("aboutButton");
-const projectButton = document.getElementById("betaButton");
+const projectButton = document.getElementById("projectButton");
 const contactsButton = document.getElementById("gammaButton");
-
-const aboutHidden = document.getElementById("aboutHidden");
-const heroHidden = document.getElementById("heroHidden");
-const projectsHidden = document.getElementById("projectsHidden");
 
 const heroVis = document.getElementById("heroVis");
 const aboutVis = document.getElementById("aboutVis");
@@ -17,6 +13,10 @@ aboutButton.addEventListener("click", () => {
     aboutHiddenAbout.classList.add("hidden");
     heroHiddenAbout.classList.add("hidden");
     projectsHiddenAbout.classList.add("hidden");
+
+    aboutHiddenProject.classList.add("hidden");
+    heroHiddenProject.classList.add("hidden");
+    projectHiddenProject.classList.add("hidden");
 
     heroVis.classList.remove("hidden");
     aboutVis.classList.remove("hidden");
@@ -35,6 +35,41 @@ aboutButton.addEventListener("click", () => {
     aboutVisible = true;
   }
 });
+
+let projectVisible = null;
+
+
+projectButton.addEventListener("click", () => {
+    if (projectVisible === true) {
+    aboutHiddenAbout.classList.add("hidden");
+    heroHiddenAbout.classList.add("hidden");
+    projectsHiddenAbout.classList.add("hidden");
+
+    aboutHiddenProject.classList.add("hidden");
+    heroHiddenProject.classList.add("hidden");
+    projectHiddenProject.classList.add("hidden");
+
+    heroVis.classList.remove("hidden");
+    aboutVis.classList.remove("hidden");
+    projectsVis.classList.remove("hidden");
+
+    projectVisible = false;
+  } else {
+    aboutHiddenProject.classList.remove("hidden");
+    heroHiddenProject.classList.remove("hidden");
+    projectsHiddenProject.classList.remove("hidden");
+
+    heroVis.classList.add("hidden");
+    aboutVis.classList.add("hidden");
+    projectsVis.classList.add("hidden");
+
+    projectVisible = true;
+  }
+});
+
+
+
+
 
 /*
 
